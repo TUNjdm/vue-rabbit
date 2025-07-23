17425f42128e8e4ccad7c0bd591a1931110fbb21
hgz <1243048280@qq.com>
2025-07-23 23:43:05 +0800

    面板组件封装

---
+++ src/apis/home.js
@@ -0,0 +1,10 @@
+import  httpInstance from '@/utils/http'
+// 获取banner
+// 定义一个名为 getBannerAPI 的函数，并将其导出，以便其他文件可以引入使用
+export function getBannerAPI() {
+    // 调用 httpInstance 函数，传入一个配置对象
+  return httpInstance({
+    // 配置请求的 URL，表明请求的是首页轮播图数据的接口
+    url: '/home/banner'
+  })
+}
--- src/views/Home/Home-index.vue
+++ src/views/Home/Home-index.vue
@@ -1,4 +1,29 @@
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import homeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
+</script>
+
 <template>
-  <div>我是home页</div>
-  <div style="height: 1200px;"></div>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <homeProduct />
+  <!-- 测试面板组件 -->
+  <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+    <div>
+      我是测试插槽
+    </div>
+  </HomePanel>
+  <HomePanel title="人气推荐" sub-title="人气推荐 爆款商品">
+    <div>
+      我是测试插槽
+    </div>
+  </HomePanel>
 </template>
