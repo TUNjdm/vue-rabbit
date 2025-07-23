25a7a2b3411883937cab880df1a5389c7f9d4a92
hgz <1243048280@qq.com>
2025-07-23 20:46:46 +0800

    piaia优化导航数据，pinia仓库category管理导航栏数据，由Layout-index统一调用，实现调用一次请求分别发送数据

--- src/main.js
+++ src/main.js
@@ -7,10 +7,6 @@ import router from './router'
 // 引入初始化样式
 import '@/styles/common.scss'
 
-import{ getCategory } from '@/apis/testAPI'
-getCategory().then(res=>{
-  console.log(res)
-})
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
---
+++ src/stores/category.js
@@ -0,0 +1,19 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import { getCategoryAPI } from '@/apis/layout';
+export const useCategoryStore = defineStore('category', () => {
+// 导航列表逻辑
+// state获取导航列表数据
+  // 使用 ref 创建一个响应式的变量 categoryList，初始值为一个空数组
+  const categoryList = ref([])
+  // action获取导航列表数据的方法
+  const getCategory= async()=>{
+    // 调用 getCategoryAPI 函数获取导航列表数据
+  const res = await getCategoryAPI()
+  categoryList.value = res.result
+}
+return {
+  categoryList,
+  getCategory
+}
+})
